07cdd1c31f78a12a67e5f2de158993b15fe204c0
“Erno <ernohilarion@MacBook-Air-de-Erno.local>
2024-04-29 17:18:43 +0200

    moka

--- index.js
+++ index.js
@@ -1,17 +1,57 @@
 class SortedList {
-  constructor() {}
+  constructor(item, length) {
+    this.items = []
+    this.length = this.items.length
+  }
 
-  add(item) {}
 
-  get(pos) {}
+  add(item) {
+    this.items.push(item)
+    this.items.sort((a, b) => a - b)
+    this.length = this.items.length
+  }
 
-  max() {}
 
-  min() {}
+  get(pos) {
+    if (pos < 0 || pos >= this.length) {
+      throw new Error("OutOfBounds");
+    } else
+      return this.items[pos];
+  }
 
-  sum() {}
 
-  avg() {}
+  max() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return Math.max(...this.items);
+    }
+  }
+
+  min() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return Math.min(...this.items);
+    }
+  }
+
+  sum() {
+    const sum = this.items.reduce((acc, currentValue) => {
+      return acc + currentValue;
+    }, 0);
+    return sum;
+  }
+
+
+  avg() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return this.sum() / this.length;
+    }
+  }
 }
+
 
 module.exports = SortedList;
